4b0d9807b1f292e7ac59348e5fe168d8cd98dbe8
Lindsay <lindsaytseng777@gmail.com>
2018-08-22 15:56:05 +0800

    different image report for img files

--- uploads/core/views.py
+++ uploads/core/views.py
@@ -127,6 +127,8 @@ def upload_dcm(request):
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
         myfile = fs.save(myfile.name, myfile)
+        fileName = list(myfile)[:-4] # remove '.dcm'
+        fileName = ''.join(fileName)
 
         # move file form media/ to media/dcm/ folder
         shutil.move('media/'+myfile, 'media/DCM/'+myfile)
@@ -148,10 +150,10 @@ def upload_dcm(request):
             del age_list[0]
         age = ''.join(age_list)
         # get MP
-        name = str(dataset.PatientName)
-        if "(PM" in name:
-            name = name.split('(')[1].split(')')[0]
-            name_list = list(name)
+        PatientName = str(dataset.PatientName)
+        if "(PM" in PatientName:
+            PatientName = PatientName.split('(')[1].split(')')[0]
+            name_list = list(PatientName)
             del name_list[0:2]
             mp = ''.join(name_list)
 
@@ -163,115 +165,94 @@ def upload_dcm(request):
             'mp': mp,
         } 
 
-        #----- get image report from file IMG00000 -----  
-        # get image and save to report
+        # ----- get image report from IMG file -----  
         # pydicom example: https://goo.gl/SMyny4
-        # zscore=[]
-        # tscore=[]
-        # if myfile.startswith('IMG00000'):
-        #     if cv2.imwrite('media/DCM/JPG/IMG00000_report.jpg', dataset.pixel_array):
-        #         # must add a '/' ahead
-        #         response['report'] = '/media/DCM/JPG/IMG00000_report.jpg'
+        if myfile.startswith('IMG'):
+            if cv2.imwrite('media/DCM/JPG/' + fileName + '_report.jpg', dataset.pixel_array):
+                # must add a '/' ahead
+                response['report'] = '/media/DCM/JPG/' + fileName + '_report.jpg'
 
 
-        comment = dataset.ImageComments
-        comment = comment.split('><')
+        # ----- get value from STR file -----
+        elif myfile.startswith('STR'):    
+            comment = dataset.ImageComments
+            comment = comment.split('><')
 
-        match = [s for s in comment if "SCAN type" in s]
-        length = len(match)
+            match = [s for s in comment if "SCAN type" in s]
+            length = len(match)
 
-        # 02 frax: major fracture
-        if length == 0:
-            keyword = [s for s in comment if "MAJOR_OSTEO_FRAC_RISK units" in s]
-            fracture = ''.join(keyword)
-            fracture = fracture.split('</')[0].split('>')[1]
-            response['fracture'] = fracture
-        # 00 01 03 04
-        else:
-            match_zscore = [s for s in comment if "BMD_ZSCORE" in s]
-            zscore=[]
-            for substring in match_zscore:
-                substring = substring.split('</')[0].split('>')[1]
-                zscore.append(substring)
-            response['zscore'] = zscore
+            # 02 frax: major fracture
+            if length == 0:
+                keyword = [s for s in comment if "MAJOR_OSTEO_FRAC_RISK units" in s]
+                fracture = ''.join(keyword)
+                fracture = fracture.split('</')[0].split('>')[1]
+                response['fracture'] = fracture
+            # 00 01 03 04
+            else:
+                match_zscore = [s for s in comment if "BMD_ZSCORE" in s]
+                zscore=[]
+                for substring in match_zscore:
+                    substring = substring.split('</')[0].split('>')[1]
+                    zscore.append(substring)
+                response['zscore'] = zscore
 
-            match_tscore = [s for s in comment if "BMD_TSCORE" in s]
-            tscore=[]
-            for substring in match_tscore:
-                substring = substring.split('</')[0].split('>')[1]
-                tscore.append(substring)
-            response['tscore'] = tscore
+                match_tscore = [s for s in comment if "BMD_TSCORE" in s]
+                tscore=[]
+                for substring in match_tscore:
+                    substring = substring.split('</')[0].split('>')[1]
+                    tscore.append(substring)
+                response['tscore'] = tscore
 
-            match_region = [s for s in comment if "ROI region" in s]
-            region=[]
-            for substring in match_region:
-                substring = substring.split('"')[1]
-                region.append(substring)
-            response['region'] = region
-            
-            # 00 01 04
-            if length == 1:
-                scanType = ''.join(match)
-                scanType = scanType.split('"')[1]
-                response['scanType'] = scanType
+                match_region = [s for s in comment if "ROI region" in s]
+                region=[]
+                for substring in match_region:
+                    substring = substring.split('"')[1]
+                    region.append(substring)
+                response['region'] = region
+                
+                # 00 01 04
+                if length == 1:
+                    scanType = ''.join(match)
+                    scanType = scanType.split('"')[1]
+                    response['scanType'] = scanType
 
-                # 04
-                if scanType == 'LVA':
-                    keyword = [s for s in comment if "DEFORMITY" in s]
-                    lva=[]
-                    for substring in keyword:
-                        substring = substring.split('</')[0].split('>')[1]
-                        lva.append(substring)
-                    while 'None' in lva:
-                        lva.remove(substring)
-                    response['lva'] = lva
+                    # 04
+                    if scanType == 'LVA':
+                        keyword = [s for s in comment if "DEFORMITY" in s]
+                        lva=[]
+                        for substring in keyword:
+                            substring = substring.split('</')[0].split('>')[1]
+                            lva.append(substring)
+                        while 'None' in lva:
+                            lva.remove(substring)
+                        response['lva'] = lva
 
-                # 00
-                elif scanType == 'AP Spine':
-                    APSpine = list(zip(region, tscore, zscore))
-                    response['APSpine'] = APSpine
+                    # 00
+                    elif scanType == 'AP Spine':
+                        APSpine = list(zip(region, tscore, zscore))
+                        response['APSpine'] = APSpine
 
-                # 01
-                elif scanType == 'DualFemur':
-                    DualFemur = list(zip(region, tscore, zscore))
-                    response['DualFemur'] = DualFemur
+                    # 01
+                    elif scanType == 'DualFemur':
+                        DualFemur = list(zip(region, tscore, zscore))
+                        response['DualFemur'] = DualFemur
 
-                else:
-                    print('error input')
+                    else:
+                        print('error input')
 
-            # 03 combination: region tscore zscore
-            elif length == 3:
-                print('<3')
-                del region[1:4]
-                print(region)
-                combination = list(zip(region, tscore, zscore))
-                print(combination)
-                response['combination'] = combination
-                # get LVA
-                T7 = [s for s in comment if "DEFORMITY" in s]
-                T7 = ''.join(T7)
-                T7 = T7.split('</')[0].split('>')[1]
-                response['T7'] = T7
-
-        # #----- get zscore, tscore from file STR00000 -----
-        # # read STR00000
-        # elif myfile.startswith('STR00000'):       
-        #     imageComments = dataset.ImageComments.split('><')
-
-        #     # get zscore
-        #     match_zscore = [s for s in imageComments if "BMD_ZSCORE" in s]
-        #     for substring in match_zscore:
-        #         substring = substring.split('</')[0].split('>')[1]
-        #         zscore.append(substring)
-
-        #     # get tscore
-        #     match_tscore = [s for s in imageComments if "BMD_TSCORE" in s]
-        #     for substring in match_tscore:
-        #         substring = substring.split('</')[0].split('>')[1]
-        #         tscore.append(substring)
-
-        #     response['zscore'] = zscore
-        #     response['tscore'] = tscore
+                # 03 combination: region tscore zscore
+                elif length == 3:
+                    print('<3')
+                    del region[1:4]
+                    print(region)
+                    combination = list(zip(region, tscore, zscore))
+                    print(combination)
+                    response['combination'] = combination
+                    # get LVA
+                    T7 = [s for s in comment if "DEFORMITY" in s]
+                    T7 = ''.join(T7)
+                    T7 = T7.split('</')[0].split('>')[1]
+                    response['T7'] = T7   
 
         uploaded_file_url = fs.url(myfile)
         response['uploaded_file_url'] = uploaded_file_url
